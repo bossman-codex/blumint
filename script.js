90e26d6158d2388e40063e4067c44e763df14779
bossman-codex <okparathankgod@gmail.com>
2023-06-11 11:46:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+// const toggleButton = document.getElementById("theme-toggle");
+// const body = document.body;
+// const svg = document.getElementById("mySvg");
+// const paths = svg.querySelectorAll("path");
+
+// // toggleButton.addEventListener("click", function () {
+
+// //     if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+// //         document.documentElement.setAttribute('data-bs-theme','light')
+// //         paths.forEach(function (path) {
+// //         path.setAttribute("fill", "black");
+// //       });
+// //     localStorage.setItem("theme", "light");
+// //     }
+// //     else {
+// //         document.documentElement.setAttribute('data-bs-theme','dark')
+// //         paths.forEach(function (path) {
+// //           path.setAttribute("fill", "white");
+// //         });
+// //         localStorage.setItem("theme", "dark");
+// //       }
+// //     }
+
+
+
+
+
+
+// //   if (body.classList.contains("data-bs-theme")) {
+// //     // Switch to light mode
+// //     body.classList.remove("dark-theme");
+    
+// //   } else {
+// //     // Switch to dark mode
+// //     body.classList.add("dark-theme");
+    
+//  //}
+//  );
+
+// // Check for user preference in localStorage
+// // const savedTheme = localStorage.getItem("theme");
+// // if (savedTheme === "dark") {
+// //   document.documentElement.setAttribute('data-bs-theme','dark')
+// //   //body.classList.add("dark-theme");
+// //   toggleButton.checked = true; // Set the toggle switch to "on"
+// //   paths.forEach(function (path) {
+// //     path.setAttribute("fill", "white");
+// //   });
+// // }
+
+// // Show the page content after the theme is fully loaded
+// window.addEventListener("load", function () {
+//   body.style.visibility = "visible";
+// });
+
+
+// window.addEventListener('scroll', function() {
+//   var navbar = document.querySelector('.navbar');
+//   if (window.pageYOffset > 0) {
+//     navbar.classList.add('navbar-solid');
+//   } else {
+//     navbar.classList.remove('navbar-solid');
+//   }
+// });
